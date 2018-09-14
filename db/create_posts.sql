3dd11877bf275219925a7fca021c07f96e480050
hanfamilym1 <michael@hanfamily.com>
2018-09-14 10:00:08 -0600

    finished all the setup TS: 10:00am

---
+++ db/create_posts.sql
@@ -0,0 +1,8 @@
+create table posts (
+    id serial primary key,
+    title varchar(45),
+    img text,
+    content text,
+    author_id int
+    references users (id)
+)
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const ctrl = require('./controller')
+const massive = require('massive')
 
 const app = express()
 app.use(bodyParser.json())
 
-const port = 4200
+let {
+    SERVER_PORT,
+    CONNECTION_STRING
+}=process.env
 
-app.listen(port, ()=>{console.log(`Listenin my dude on ${port}`)})
+massive(CONNECTION_STRING).then(db=>app.set('db',db))
+
+app.listen(SERVER_PORT, ()=>{console.log(`Listenin my dude on ${SERVER_PORT}`)})
 
