9d45c6fa9f1df1575af0f19a119f35803b0e4993
hanfamilym1 <michael@hanfamily.com>
2018-09-14 09:41:10 -0600

    Finished Setup for React TS: 9:41

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Auth from './Components/Auth/Auth'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+import Nav from './Components/Nav/Nav'
+import Post from './Components/Post/Post'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav/>
+        <Auth/>
+        <Dashboard/>
+        <Form/>
+        <Post/>
       </div>
     );
   }
